b5df7a60eaf9c07217f05914c9200ba821e87a58
rigo <mbandrigo@gmail.com>
2019-07-29 08:08:21 +0800

    admin responsive menu

--- admin/data-pelamar.php
+++ admin/data-pelamar.php
@@ -164,6 +164,30 @@ $hasil = mysqli_query($db, $query);
             </div>
         </div>
     </div>
+    
+
+    <div class="mobile-menu-area">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                            <div class="mobile-menu">
+                                <nav id="dropdown">
+                                    <ul class="mobile-menu-nav">
+                                        <li><a data-toggle="collapse" data-target="#Charts" href="http://localhost/andi_offset/admin/">Dashboard <span class="admin-project-icon adminpro-icon adminpro-down-arrow"></span></a>
+                                          
+                                        </li>
+                                        <li><a data-toggle="collapse" data-target="#demo" href="http://localhost/andi_offset/admin/data-pelamar.php">Daftar Pelamar <span class="admin-project-icon adminpro-icon adminpro-down-arrow"></span></a>
+                                            
+                                        </li>
+                                        
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
 
     <!-- Mobile Menu end -->
     <div class="breadcome-area">
